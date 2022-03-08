0f45dbf7bd56971e59535f2630474d2739ee9284
venzhi87 <anarhy87@list.ru>
2022-03-08 17:52:46 +0300

    did it without User Greating

---
+++ CheckLogInAndPass_HW3/GreatingViewController.swift
@@ -0,0 +1,18 @@
+//
+//  GreatingViewController.swift
+//  CheckLogInAndPass_HW3
+//
+//  Created by Pavel Kuzovlev on 08.03.2022.
+//
+
+import UIKit
+
+class GreatingViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+
+    }
+
+}
--- CheckLogInAndPass_HW3/ViewController.swift
+++ CheckLogInAndPass_HW3/ViewController.swift
@@ -8,12 +8,53 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    @IBOutlet var userNameTextField: UITextField!
+    @IBOutlet var passwordTextField: UITextField!
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+     
     }
-
+    @IBAction func logInButtonPressed(_ sender: Any) {
+        
+        let userNameSet = "Pavel"
+        let passwordSet = "PASSWORD"
+        
+        if userNameTextField.text != userNameSet || passwordTextField.text != passwordSet {
+            let alertController = UIAlertController(title: "Password Incorrect", message: "Please re-type password", preferredStyle: .alert)
+            let action = UIAlertAction(title: "ok", style: .default) { (action) in
+            }
+            alertController.addAction(action)
+            self.present(alertController, animated: true, completion: nil)
+            
+            
+        }
+    }
+    
+    
+    @IBAction func ShowUserName(_ sender: Any) {
+        let alertController = UIAlertController(title: "Oops!", message: "Your name is Pavel 😘", preferredStyle: .alert)
+        let action = UIAlertAction(title: "ok", style: .default) { (action) in
+        }
+        alertController.addAction(action)
+        self.present(alertController, animated: true, completion: nil)
+    }
+    
+    @IBAction func ShowPassword(_ sender: Any) {
+        let alertController = UIAlertController(title: "Oops!", message: "Your password is PASSWORD 😘", preferredStyle: .alert)
+        let action = UIAlertAction(title: "ok", style: .default) { (action) in
+        }
+        alertController.addAction(action)
+        self.present(alertController, animated: true, completion: nil)
+    }
+    
+    @IBAction func unwind(for segue: UIStoryboardSegue) {
+        guard segue.source is GreatingViewController else { return }
+        userNameTextField.text = ""
+        passwordTextField.text = ""
+    }
 
 }
 
