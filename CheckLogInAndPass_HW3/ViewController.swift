e504fdd2231ac28224e8c4a01577215ac79163a5
venzhi87 <anarhy87@list.ru>
2022-03-08 11:16:16 +0300

    Initial Commit

---
+++ CheckLogInAndPass_HW3/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  CheckLogInAndPass_HW3
+//
+//  Created by Pavel Kuzovlev on 08.03.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
